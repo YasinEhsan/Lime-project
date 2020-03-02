80d50f2fdbfafbc19fda02afae8de61eda998858
yasinehsan <yasinehsan11@gmail.com>
2020-03-02 09:19:48 -0500

    adde pics

--- exp.py
+++ exp.py
@@ -1,12 +1,8 @@
-sample_scooters = runModel()
-plot([x[0] for x in sample_scooters], [x[1] for x in sample_scooters], label='')
-def decorate_limeshare():
-    """Add a title and label the axes."""
-    decorate(title='variable *number_of_scooters*, fixed  *number_of_region*',
-             xlabel='number_of_scooters',
-             ylabel='send_truck')
-print([x[0] for x in sample_scooters])
-print([x[1] for x in sample_scooters])
-print([x[2] for x in sample_scooters])
-print([x[3] for x in sample_scooters])
-decorate_limeshare()
+[("Forest Hills", 1664),
+               ("Jamaica", 1709),
+               (Richmond Hill, 1171
+Ozone Park	576
+Queens Village	1611
+Astoria	902
+Colleg Point	1148
+Woodside	649)]
